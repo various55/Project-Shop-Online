f35debc42a941f5cdc570349f3cbca0db1aa22c8
lanhuong <you@example.com>
2021-05-05 18:30:48 +0700

    add fe-v1

---
+++ src/ShopOnline/ShopOnline/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopOnline.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ProductDetail()
+        {
+            return View();
+        }
+    }
+}
