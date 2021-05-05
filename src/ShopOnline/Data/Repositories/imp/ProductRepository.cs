270b0b7731685dfb37f4ea34ab349a158e3a3cdd
vanh55 <truongvietanh221@gmail.com>
2021-05-06 03:01:15 +0700

    Thêm tầng data - repository

---
+++ src/ShopOnline/Data/Repositories/imp/ProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repositories.imp
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+
+    }
+    public class ProductRepository : RepositoryBase<Product>, IProductRepository
+    {
+        public ProductRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+
+        }
+
+    }
+}
---
+++ src/ShopOnline/Data/Repositories/imp/RoleRepository.cs
@@ -0,0 +1,22 @@
+﻿using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repositories.imp
+{
+    public interface IRoleRepository : IRepository<Role>
+    {
+
+    }
+    public class RoleRepository : RepositoryBase<Role>, IRoleRepository
+    {
+        public RoleRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+
+        }
+
+    }
+}
