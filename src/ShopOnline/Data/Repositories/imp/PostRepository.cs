a41ac04ab352d1477cbb16c2ac27fc95bcdf9186
vanh55 <truongvietanh221@gmail.com>
2021-05-06 03:01:15 +0700

    Thêm tầng data - repository

---
+++ src/ShopOnline/Data/Repositories/imp/PostRepository.cs
@@ -0,0 +1,22 @@
+﻿using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repositories.imp
+{
+    public interface IPostRepository : IRepository<Post>
+    {
+
+    }
+    public class PostRepository : RepositoryBase<Post>, IPostRepository
+    {
+        public PostRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+
+        }
+
+    }
+}
