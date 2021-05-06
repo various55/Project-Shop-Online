2108f50d609e152216e1875f7d0de36b5698ed5c
vanh55 <truongvietanh221@gmail.com>
2021-05-06 23:41:37 +0700

    add file hướng dẫn làm

---
+++ src/ShopOnline/Business/Services/BaseService.cs
@@ -0,0 +1,80 @@
+﻿using Data.Models;
+using Data.Repositories;
+using Data.Repositories.imp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Services
+{
+    // File ví dụ hướng dẫn làm
+    public interface IOrderService
+    {
+        ICollection<Order> FindAll();
+        ICollection<Order> FindByUserId(int id);
+        ICollection<Order> FindByEmail(string email);
+        ICollection<Order> FindByTotal(float total);
+        bool Add(Order order);
+        bool Remove(int id);
+    }
+    public class BaseService : IOrderService
+    {
+        public IOrderRepository OrderRepository;
+        public IUserRepository userRepository;
+
+        public IUnitOfWork unitOfWork;
+
+        public BaseService()
+        {
+
+        }
+        public BaseService(IOrderRepository OrderRepository,IUserRepository userRepository, IUnitOfWork unitOfWork)
+        {
+            this.OrderRepository = OrderRepository;
+            this.unitOfWork = unitOfWork;
+            this.userRepository = userRepository;
+        }
+
+
+        public ICollection<Order> FindAll()
+        {
+            return OrderRepository.findAll(new string[] { "UserMember" });
+        }
+        // Tìm tất cả các order của user có id
+        public ICollection<Order> FindByUserId(int id)
+        {
+            // findByCondition: Tìm kiếm theo điều kiện
+            var orders = OrderRepository.findByCondition(o => o.UserID == id);
+            return orders;
+        }
+        // Tìm tất cả các order của user có email là abc@gmail.com
+        public ICollection<Order> FindByEmail(string email)
+        {
+            // findByCondition: Tìm kiếm theo điều kiện, nhưng mà email nó k nằm trong order
+            var orders = OrderRepository.findByCondition(o => o.UserMember.Email.Contains(email),new string[] { "UserMember" });
+            return orders;
+        }
+        public ICollection<Order> FindByTotal(float total)
+        {
+            // findByCondition: Tìm kiếm theo điều kiện, nhưng mà email nó k nằm trong order
+            var orders = OrderRepository.findByCondition(o => o.Total>total, new string[] { "UserMember" });
+            return orders;
+        }
+        public bool Add(Order order)
+        {
+            var res = OrderRepository.add(order);
+            return res != null;
+        }
+        public bool Remove(int id)
+        {
+            var res = OrderRepository.delete(id);
+            return res != null;
+        }
+        public void Update(Order order)
+        {
+            OrderRepository.update(order);
+        }
+    }
+}
---
+++ src/ShopOnline/ShopOnline/Controllers/BaseController.cs
@@ -0,0 +1,99 @@
+﻿using Business.Services;
+using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopOnline.Controllers
+{
+    public class BaseController : Controller
+    {
+        // Khởi tạo đối tượng tầng dịch vụ Service
+        IProductService productService;
+
+        public BaseController()
+        {
+
+        }
+        public BaseController(IProductService productService)
+        {
+            // Tiêm vào để sử dụng
+            this.productService = productService;
+        }
+
+        // GET: Base
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult Add()
+        {
+            // T đang ví dụ nên bỏ tham số truyền vào là một ProductDTO, nếu add bình thường sẽ có  tham số Add(ProductDTO productDTO)
+            // T sẽ giả data để có thể convert thử từ DTO sáng Model
+            ProductDTO productDTO = new ProductDTO();
+            productDTO.Name = "Áo sơ mi trắng nam";
+            productDTO.CategoryID = 1;
+            productDTO.SupplierID = 1;
+            productDTO.UrlImage = "images/ao2.png";
+            productDTO.Description = "Alo";
+            productDTO.ImportPrice = 50000;
+            productDTO.ExportPrice = 70000;
+            // Nhận vào DTO => model 
+
+            //if(ModelState.IsValid) -- Câu này kiểm tra hợp lệ k, nếu hợp lệ thì mới thêm vào, k thì sẽ trả về luôn
+            // Ta sẽ chuyển từ DTO ( nhận từ View ) sang Model để có thể lưu xuống db
+            Product product = new Product();
+            // Convert từ DTO(View) -> Model(Db)
+            product = AutoMapper.Mapper.Map<Product>(productDTO);
+            // Sau đấy thì chỉ việc add xuống db
+            //
+            var res = productService.Add(product);
+            // Lưu lại 1 transaction
+            if (res) productService.Save();
+            // Res trả về true nếu thành công, false nếu thất bại, gửi lên client, client sẽ hiển thị thông báo cho user
+            return new JsonResult() { Data=res,JsonRequestBehavior = JsonRequestBehavior.AllowGet};
+        }
+        //GET
+        public ActionResult Update(int id)
+        {
+            // Update cx tương tự, update sẽ có method là POST và GET
+            // GET dùng để lấy dữ liệu và đổ lên client
+            // POST sẽ nhận dữ liệu từ client và tiến hành cập nhật
+            // Đây là method GET, nhận vào 1 id và trả về đối tượng đấy
+            var product = productService.FindById(id);
+            // Chuyển nó thành productDTO để hiển thị lên view
+            ProductDTO productDTO = AutoMapper.Mapper.Map<ProductDTO>(product);
+            return View(productDTO);
+        }
+        //POST
+        [HttpPost]
+        public ActionResult Update(ProductDTO productDTO)
+        {
+            // Update cx tương tự, update sẽ có method là POST và GET
+            // GET dùng để lấy dữ liệu và đổ lên client
+            // POST sẽ nhận dữ liệu từ client và tiến hành cập nhật
+            // Đây là method GET, nhận vào 1 id và trả về đối tượng đấy
+            // Chuyển nó thành productDTO để hiển thị lên view
+            Product product = AutoMapper.Mapper.Map<Product>(productDTO);
+            productService.Update(product);
+            productService.Save();
+            return View(productDTO);
+        }
+        public ActionResult Remove()
+        {
+            return View();
+        }
+        public ActionResult FindAll()
+        {
+            var products = productService.FindAll();
+            return View();
+        }
+        public ActionResult FindInclude()
+        {
+            var products = productService.FindAll(new string[] { "Category" });
+            return View();
+        }
+    }
+}
