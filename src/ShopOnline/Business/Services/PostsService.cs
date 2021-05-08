289437010c3a929f7972ecf08b7ed7854d2e3f7a
LuuThiTuoi <81431299+LuuThiTuoi@users.noreply.github.com>
2021-05-09 02:23:50 +0700

    Tuoi add Posts

---
+++ src/ShopOnline/Business/Services/PostsService.cs
@@ -0,0 +1,74 @@
+﻿using Data.Models;
+using Data.Repositories;
+using Data.Repositories.imp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Services
+{
+    public interface IPostService
+    {
+
+        ICollection<Post> FindAll();
+      
+        Post FindById(int id);
+      
+        bool Add(Post post);
+        bool Remove(int id);
+
+        bool Update(Post post);
+        void Save();
+    }
+    public class PostService : IPostService
+    {
+        public IPostRepository PostRepository;
+        public IUserRepository userRepository;
+
+        public IUnitOfWork unitOfWork;
+        public PostService()
+        {
+
+        }
+        public PostService(IPostRepository PostRepository, IUserRepository userRepository, IUnitOfWork unitOfWork)
+        {
+            this.PostRepository = PostRepository;
+            this.unitOfWork = unitOfWork;
+            this.userRepository = userRepository;
+        }
+
+
+        public ICollection<Post> FindAll()
+        {
+            var x = PostRepository.findAll();
+            return PostRepository.findAll(new string[] { "user" });
+        }
+        public Post FindById(int id)
+        {
+            return PostRepository.findById(id);
+        }
+       
+      
+        public bool Add(Post post)
+        {
+            var res = PostRepository.add(post);
+            return res != null;
+        }
+        public bool Remove(int id)
+        {
+            var res = PostRepository.delete(id);
+            return res != null;
+        }
+        public bool Update(Post post)
+        {
+            return PostRepository.update(post);
+        }
+
+        public void Save()
+        {
+            unitOfWork.commit();
+        }
+    }
+}
